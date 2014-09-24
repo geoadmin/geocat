9d440facbc9100ec5fee2c522cabc09a8f6aae58
Florent gravin <florent.gravin@camptocamp.com>
2014-09-24 17:14:23 +0200

    Fix geocat merge

--- web-ui/src/main/resources/catalog/js/custom/geocat/geocat.js
+++ web-ui/src/main/resources/catalog/js/custom/geocat/geocat.js
@@ -30,6 +30,9 @@
         advancedMode: false,
         searchMap: gnSearchSettings.searchMap
       });
+
+      proj4.defs("EPSG:21781","+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=660.077,13.551,369.344,2.484,1.783,2.939,5.66 +units=m +no_defs");
+
     }]);
 
     module.controller('gocatSearchFormCtrl', [
@@ -195,7 +198,7 @@
       $('#keywordsF').tagsinput('input').typeahead({
         valueKey: 'label',
         prefetch: {
-          url :gnHttpServices.info,
+          url :gnHttpServices.geocatKeywords,
           filter: function(data) {
             var res = [];
             for(var i=0; i<data.metadatacategory.length;i++) {
