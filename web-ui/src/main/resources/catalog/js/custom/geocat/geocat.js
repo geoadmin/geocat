d71b2bd37196725e4d8026c0e05698ff2982e4bc
Florent gravin <florent.gravin@camptocamp.com>
2014-09-26 12:27:49 +0200

    Fix geocat from merge

--- web-ui/src/main/resources/catalog/js/custom/geocat/geocat.js
+++ web-ui/src/main/resources/catalog/js/custom/geocat/geocat.js
@@ -128,40 +128,46 @@
       });
 
       /** Manage cantons selection (add feature to the map) */
+      var cantonSource = new ol.source.Vector();
       var nbCantons = 0;
+      var cantonVector = new ol.layer.Vector({
+        source: cantonSource,
+        style: gnSearchSettings.olStyles.drawBbox
+      });
       var addCantonFeature = function(id) {
-        var url = 'http://www.geocat.ch/geonetwork/srv/eng/region.geom.wkt?id=kantone:'+id+'&srs=EPSG:3857';
-        var proxyUrl = '../../proxy?url=' + encodeURIComponent(url);
         nbCantons++;
 
-        return $http.get(proxyUrl).success(function(wkt) {
+        return gnHttp.callService('regionWkt', {
+          id: id,
+          srs: 'EPSG:21781'
+        }).success(function(wkt) {
           var parser = new ol.format.WKT();
           var feature = parser.readFeature(wkt);
-          featureOverlay.getFeatures().push(feature);
+          feature.setGeometry(feature.getGeometry().transform('EPSG:21781', 'EPSG:3857'));
+          cantonSource.addFeature(feature);
         });
       };
+      map.addLayer(cantonVector);
 
       // Request cantons geometry and zoom to extent when
       // all requests respond.
-      $scope.$watch('searchObj.params.cantons', function(v){
-        featureOverlay.getFeatures().clear();
+      var onRegionSelect = function(v){
+        cantonSource.clear();
         if(angular.isDefined(v) && v != '') {
           var cs = v.split(',');
           for(var i=0; i<cs.length;i++) {
-            var id = cs[i].split('#')[1];
-            addCantonFeature(Math.floor((Math.random() * 10) + 1)).then(function(){
+            addCantonFeature(cs[i]).then(function(){
               if(--nbCantons == 0) {
-                var features = featureOverlay.getFeatures();
-                var extent = features.item(0).getGeometry().getExtent();
-                features.forEach(function(f) {
-                  ol.extent.extend(extent, f.getGeometry().getExtent());
-                });
-                map.getView().fitExtent(extent, map.getSize());
+                map.getView().fitExtent(cantonSource.getExtent(), map.getSize());
               }
             });
           }
         }
-      });
+      };
+
+      $scope.$watch('searchObj.params.countries', onRegionSelect);
+      $scope.$watch('searchObj.params.cantons', onRegionSelect);
+      $scope.$watch('searchObj.params.cities', onRegionSelect);
 
       var key;
       var format = new ol.format.WKT();
