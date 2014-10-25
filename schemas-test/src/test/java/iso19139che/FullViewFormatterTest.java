6f49f22aa2e22e63ceffdf82d7bcac5193515cd8
Jesse Eichar <jesse.eichar@camptocamp.com>
2014-10-25 08:45:47 +0200

    added iso19139che full view and test

--- schemas-test/src/test/java/iso19139che/FullViewFormatterTest.java
+++ schemas-test/src/test/java/iso19139che/FullViewFormatterTest.java
@@ -1,12 +1,22 @@
 package iso19139che;
 
 import com.google.common.collect.Lists;
+import com.google.common.io.Files;
+import org.fao.geonet.Constants;
+import org.fao.geonet.languages.IsoLanguagesMapper;
+import org.fao.geonet.repository.IsoLanguageRepository;
 import org.fao.geonet.services.metadata.format.AbstractFormatterTest;
+import org.fao.geonet.services.metadata.format.FormatterParams;
+import org.fao.geonet.services.metadata.format.groovy.Environment;
+import org.fao.geonet.services.metadata.format.groovy.EnvironmentImpl;
+import org.fao.geonet.services.metadata.format.groovy.Functions;
 import org.fao.geonet.utils.Xml;
+import org.jdom.Attribute;
 import org.jdom.Content;
 import org.jdom.Element;
 import org.jdom.Text;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 import java.io.File;
@@ -17,6 +27,10 @@ import java.util.List;
  */
 public class FullViewFormatterTest extends AbstractFormatterTest {
 
+    @Autowired
+    private IsoLanguagesMapper mapper;
+    @Autowired
+    private IsoLanguageRepository langRepo;
     @Test
     public void testBasicFormat() throws Exception {
 
@@ -24,24 +38,42 @@ public class FullViewFormatterTest extends AbstractFormatterTest {
         request.addParameter("html", "true");
 
         final String formatterId = "full_view";
+        FormatterParams fparams = getFormatterFormatterParamsPair
+                (request, formatterId).two();
+        Environment env = new EnvironmentImpl(fparams, mapper);
+        final Functions functions = new Functions(fparams, env, langRepo);
 
 //        measureFormatterPerformance(request, formatterId);
 
         final String view = formatService.exec("eng", "html", "" + id, null, formatterId, "true", false, request);
-//        Files.write(view, new File("e:/tmp/view.html"), Constants.CHARSET);
+        Files.write(view, new File("e:/tmp/view.html"), Constants.CHARSET);
 
         List<String> excludes = Lists.newArrayList(
                 "> gmd:MD_Metadata > gmd:identificationInfo > gmd:MD_DataIdentification > gmd:citation > gmd:CI_Citation > gmd:title > " +
                 "gco:PT_FreeText > gco:textGroup > gmd:LocalisedCharacterString > Text"
         );
 
-        final List<?> text = Xml.selectNodes(Xml.loadString(xml, false), "*//text()");
+        final Element xmlEl = Xml.loadString(xml, false);
+        final List text = Lists.newArrayList(Xml.selectNodes(xmlEl, "*//node()[not(@codeList)]/text()"));
+        text.addAll(Lists.newArrayList(Xml.selectNodes(xmlEl, "*//node()[@codeList]/@codeListValue")));
+
         StringBuilder missingStrings = new StringBuilder();
         for (Object t : text) {
-            Text textEl = (Text) t;
-            final String requiredText = textEl.getTextTrim();
+            final String path;
+            final String requiredText;
+            if (t instanceof Text) {
+                requiredText = ((Text) t).getTextTrim();
+                path = getXPath((Content) t).trim();
+            } else if (t instanceof Attribute) {
+                Attribute attribute = (Attribute) t;
+                final String codelist = attribute.getParent().getAttributeValue("codeList");
+                final String code = attribute.getValue();
+                requiredText = functions.codelistValueLabel(codelist, code);
+                path = getXPath(attribute.getParent()).trim() + "> @codeListValue";
+            } else {
+                throw new AssertionError(t.getClass() + " is not handled");
+            }
             if (!requiredText.isEmpty() && !view.contains(requiredText)) {
-                final String path = getXPath(textEl).trim();
                 if (!excludes.contains(path)) {
                     missingStrings.append("\n").append(path).append(" -> ").append(requiredText);
                 }
