a12299551bfb64d618a93bff8adc0e540d71d5fa
Jesse Eichar <jesse.eichar@camptocamp.com>
2014-10-29 14:19:57 +0100

    remove id column in shared objects because it doesn't make much sense with subtemplates

--- src/main/plugin/iso19139.che/formatter/xml_view/view.groovy
+++
@@ -1,5 +0,0 @@
-handlers.add name: 'full xml', select: 'che:CHE_MD_Metadata', { el ->
-    // Don't need a return because last expression of a function is
-    // always returned in groovy
-    el
-}
