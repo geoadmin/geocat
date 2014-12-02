119b4db7bd080db321a2ad8ac2186e7926e0584f
Florent gravin <florent.gravin@camptocamp.com>
2014-12-02 15:52:28 +0100

    update worldExtent of ch proj fix md bbox drawn on map

--- web-ui/src/main/resources/catalog/js/custom/geocat/config.js
+++ web-ui/src/main/resources/catalog/js/custom/geocat/config.js
@@ -10,7 +10,7 @@
 
       proj4.defs("EPSG:21781","+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=660.077,13.551,369.344,2.484,1.783,2.939,5.66 +units=m +no_defs");
       ol.proj.get('EPSG:21781').setExtent([420000, 30000, 900000, 350000]);
-      ol.proj.get('EPSG:21781').setWorldExtent([420000, 30000, 900000, 350000]);
+      ol.proj.get('EPSG:21781').setWorldExtent([5.9700, 45.8300, 10.4900, 47.8100]);
 
       searchSettings.olStyles = {
         drawBbox: new ol.style.Style({
@@ -24,17 +24,17 @@
         }),
         mdExtent: new ol.style.Style({
           stroke: new ol.style.Stroke({
-            color: 'orange',
+            color: 'black',
             width: 2
           })
         }),
         mdExtentHighlight:new ol.style.Style({
           stroke: new ol.style.Stroke({
-            color: 'orange',
+            color: 'black',
             width: 3
           }),
           fill: new ol.style.Fill({
-            color: 'rgba(255,255,0,0.3)'
+            color: 'rgba(0,0,0,0.3)'
           })
         })
       };
