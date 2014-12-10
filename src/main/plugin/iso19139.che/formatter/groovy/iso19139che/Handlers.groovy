0f26a926e759e4036ddb7d8d6cc99ac5bb9d24b2
Jesse Eichar <jesse.eichar@camptocamp.com>
2014-12-10 13:01:19 +0100

    update che formatter handler

--- src/main/plugin/iso19139.che/formatter/groovy/iso19139che/Handlers.groovy
+++ src/main/plugin/iso19139.che/formatter/groovy/iso19139che/Handlers.groovy
@@ -4,8 +4,12 @@ public class Handlers extends iso19139.Handlers {
 
     public Handlers(handlers, f, env) {
         super(handlers, f, env);
+        this.extentMapBackground = 'geocat'
+        this.rootEl = 'che:CHE_MD_Metadata'
+        this.extentMapProjection = "EPSG:21781"
         this.packageViews.remove('gmd:MD_Metadata')
         this.packageViews << 'che:legislationInformation'
-        this.packageViews << 'che:CHE_MD_Metadata'
+        this.packageViews << this.rootEl
     }
+
 }
