bd589032e5b94d612cb347f1ad58bdf2f75773eb
Jesse Eichar <jesse.eichar@camptocamp.com>
2014-12-16 17:28:38 +0100

    migrate schematroncriteria table

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v2110/0-geocat-postgres.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v2110/0-geocat-postgres.sql
@@ -5,6 +5,7 @@ ALTER TABLE schematron ADD COLUMN displaypriority integer;
 UPDATE schematron SET displaypriority=0;
 ALTER TABLE schematron ALTER displaypriority SET NOT NULL;
 ALTER TABLE schematron RENAME COLUMN file TO filename;
+UPDATE schematroncriteria SET uitype='Group' where type = 'GROUP';
 
 -- ALTER TABLE requests RENAME TO requests_v2;
 -- ALTER TABLE params RENAME TO params_v2;
