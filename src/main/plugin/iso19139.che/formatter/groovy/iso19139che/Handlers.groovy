c96f16f8fa16b25575c51e239c12c062eebdbce5
Jesse Eichar <jesse.eichar@camptocamp.com>
2014-12-19 14:43:37 +0100

    Merge branch 'refactor_search' of github.com:geonetwork/core-geonetwork into geocat_searchui_develop
    Conflicts:
    	bin/start.ps1
    	schemas/iso19139/src/main/plugin/iso19139/formatter/groovy/iso19139/Handlers.groovy

--- src/main/plugin/iso19139.che/formatter/groovy/iso19139che/Handlers.groovy
+++ src/main/plugin/iso19139.che/formatter/groovy/iso19139che/Handlers.groovy
@@ -4,9 +4,7 @@ public class Handlers extends iso19139.Handlers {
 
     public Handlers(handlers, f, env) {
         super(handlers, f, env);
-        this.extentMapBackground = 'geocat'
         this.rootEl = 'che:CHE_MD_Metadata'
-        this.extentMapProjection = "EPSG:21781"
         this.packageViews.remove('gmd:MD_Metadata')
         this.packageViews << 'che:legislationInformation'
         this.packageViews << this.rootEl
