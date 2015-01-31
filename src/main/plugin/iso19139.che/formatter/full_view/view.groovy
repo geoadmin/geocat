210ac94c5e0df2806e04b04ed9c76e2bb8b862f8
Jesse Eichar <jesse.eichar@camptocamp.com>
2015-01-31 22:08:39 +0100

    Proper fix for individual name.

--- src/main/plugin/iso19139.che/formatter/full_view/view.groovy
+++ src/main/plugin/iso19139.che/formatter/full_view/view.groovy
@@ -1,11 +1,6 @@
 import iso19139.SummaryFactory
 
 def isoHandlers = new iso19139che.Handlers(handlers, f, env)
-
-def factory = new SummaryFactory(isoHandlers, isoHandlers.summaryCustomizer)
-factory.summaryCustomizer = isoHandlers.summaryCustomizer
-
-factory.handlers.add name: "Summary Handler", select: {it.parent() is it.parent()}, {factory.create(it).getResult()}
-
+SummaryFactory.summaryHandler({it.parent() is it.parent()}, isoHandlers)
 
 isoHandlers.addDefaultHandlers()
