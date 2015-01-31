c51df9c39448b34e1f0de40b639992953e5cf3c3
Jesse Eichar <jesse.eichar@camptocamp.com>
2015-01-31 22:08:39 +0100

    Proper fix for individual name.

--- schemas/iso19139.che/src/main/plugin/iso19139.che/formatter/groovy/iso19139che/Handlers.groovy
+++ schemas/iso19139.che/src/main/plugin/iso19139.che/formatter/groovy/iso19139che/Handlers.groovy
@@ -1,17 +1,6 @@
 package iso19139che
 
-import org.fao.geonet.services.metadata.format.groovy.util.MenuAction
-import org.fao.geonet.services.metadata.format.groovy.util.Summary
-
 public class Handlers extends iso19139.Handlers {
-
-    Closure<Summary> summaryCustomizer = {summary ->
-
-        List submenu = summary.findAction("export").submenu
-        submenu.add(1,  new MenuAction(label: "exportIso19139", javascript: "window.open('xml_iso19139?uuid=${this.env.metadataUUID}', '_blank')", iconClasses: "fa fa-file-code-o"),)
-        submenu.add(2,  new MenuAction(label: "exportGM03", javascript: "window.open('gm03.xml?uuid=${this.env.metadataUUID}', '_blank')", iconClasses: "fa fa-file-code-o"),)
-    }
-
     public Handlers(handlers, f, env) {
         super(handlers, f, env);
         isofunc = new Functions(handlers: handlers, f:f, env:env, commonHandlers: commonHandlers)
@@ -31,7 +20,7 @@ public class Handlers extends iso19139.Handlers {
         ]
         def nameString = party.'che:individualFirstName'.text() + " " + party.'che:individualLastName'.text()
         def name = ""
-        if (name.isEmpty()) {
+        if (!nameString.trim().isEmpty()) {
             name = commonHandlers.func.textEl(f.nodeLabel('gmd:individualName', null), nameString)
         }
         def childData = name.toString() + handlers.processElements(generalChildren)
