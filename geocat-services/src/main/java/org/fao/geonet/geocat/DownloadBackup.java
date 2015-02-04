2c3c447a76f5d894dadbafcf7933440a55719ebd
Jesse Eichar <jesse.eichar@camptocamp.com>
2015-02-04 08:09:41 +0100

    Fix for broken download back up service

--- geocat-services/src/main/java/org/fao/geonet/geocat/DownloadBackup.java
+++ geocat-services/src/main/java/org/fao/geonet/geocat/DownloadBackup.java
@@ -1,22 +1,26 @@
 package org.fao.geonet.geocat;
 
 import jeeves.server.context.ServiceContext;
+import jeeves.server.dispatchers.ServiceManager;
 import org.fao.geonet.exceptions.JeevesException;
 import org.fao.geonet.kernel.GeonetworkDataDirectory;
 import org.fao.geonet.utils.Log;
-import org.jdom.Element;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.File;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class DownloadBackup {
-
+    @Autowired
+    ServiceManager serviceManager;
     @RequestMapping(value="/{lang}/download.backup")
-    public FileSystemResource exec(Element params, ServiceContext context)
-            throws Exception {
+    public FileSystemResource exec(@PathVariable String lang, HttpServletRequest request) throws Exception {
+        ServiceContext context = serviceManager.createServiceContext("download.backup", lang, request);
         Log.info(ArchiveAllMetadataJob.BACKUP_LOG, "User " + context.getUserSession().getUsername() + " from IP: " + context
                 .getIpAddress() + " has started to download backup archive");
         String datadir = System.getProperty(GeonetworkDataDirectory.GEONETWORK_DIR_KEY);
