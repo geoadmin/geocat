0ee5c16a397d0cac1c8016fd50fd9d2851bfc998
Jesse Eichar <jesse.eichar@camptocamp.com>
2015-03-31 15:52:50 +0200

    show all elements in contacts that are required

--- web-ui/src/main/resources/catalog/geocat-shared-objects/js/shared-object-subtemplate-service.js
+++ web-ui/src/main/resources/catalog/geocat-shared-objects/js/shared-object-subtemplate-service.js
@@ -35,52 +35,66 @@
               data: data
             })
           },
-          contactTemplate: '<che:CHE_CI_ResponsibleParty xmlns:che="http://www.geocat.ch/2008/che" xmlns:gco="http://www.isotc211.org/2005/gco" xmlns:gmd="http://www.isotc211.org/2005/gmd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" gco:isoType="gmd:CI_ResponsibleParty">\n' +
-          '   <gmd:organisationName xsi:type="gmd:PT_FreeText_PropertyType">\n' +
-          '      <gmd:PT_FreeText>\n' +
-          '         <gmd:textGroup>\n' +
-          '            <gmd:LocalisedCharacterString locale="#DE">~~ Template First Name ~~</gmd:LocalisedCharacterString>\n' +
-          '         </gmd:textGroup>\n' +
-          '         <gmd:textGroup>\n' +
-          '            <gmd:LocalisedCharacterString locale="#FR">~~ Template First Name ~~</gmd:LocalisedCharacterString>\n' +
-          '         </gmd:textGroup>\n' +
-          '         <gmd:textGroup>\n' +
-          '            <gmd:LocalisedCharacterString locale="#IT">~~ Template First Name ~~</gmd:LocalisedCharacterString>\n' +
-          '         </gmd:textGroup>\n' +
-          '         <gmd:textGroup>\n' +
-          '            <gmd:LocalisedCharacterString locale="#EN">~~ Template First Name ~~</gmd:LocalisedCharacterString>\n' +
-          '         </gmd:textGroup>\n' +
-          '      </gmd:PT_FreeText>\n' +
-          '   </gmd:organisationName>\n' +
-          '   <gmd:contactInfo>\n' +
-          '      <gmd:CI_Contact>\n' +
-          '         <gmd:address>\n' +
-          '            <che:CHE_CI_Address gco:isoType="gmd:CI_Address">\n' +
-          '               <gmd:electronicMailAddress>\n' +
-          '                  <gco:CharacterString>~~ Template Email ~~</gco:CharacterString>\n' +
-          '               </gmd:electronicMailAddress>\n' +
-          '            </che:CHE_CI_Address>\n' +
-          '         </gmd:address>\n' +
-          '      </gmd:CI_Contact>\n' +
-          '   </gmd:contactInfo>\n' +
-          '   <gmd:role>\n' +
-          '      <gmd:CI_RoleCode codeList="http://www.isotc211.org/2005/resources/codeList.xml#CI_RoleCode" codeListValue="pointOfContact" />\n' +
-          '   </gmd:role>\n' +
-          '   <che:individualFirstName>\n' +
-          '      <gco:CharacterString>~~ Template First Name ~~</gco:CharacterString>\n' +
-          '   </che:individualFirstName>\n' +
-          '   <che:individualLastName>\n' +
-          '      <gco:CharacterString>~~ Template Last Name ~~</gco:CharacterString>\n' +
-          '   </che:individualLastName>\n' +
-          '</che:CHE_CI_ResponsibleParty>',
-          formatTemplate: '<gmd:MD_Format xmlns:gmd="http://www.isotc211.org/2005/gmd" xmlns:gco="http://www.isotc211.org/2005/gco">' +
-          '  <gmd:name>' +
-          '    <gco:CharacterString >~~ Template Name ~~</gco:CharacterString>' +
-          '  </gmd:name>' +
-          '  <gmd:version>' +
-          '    <gco:CharacterString>~~ Template Version ~~</gco:CharacterString>' +
-          '  </gmd:version>' +
-          '</gmd:MD_Format>'
+          contactTemplate: '<che:CHE_CI_ResponsibleParty xmlns:che="http://www.geocat.ch/2008/che"' +
+          '                             xmlns:gco="http://www.isotc211.org/2005/gco"' +
+          '                             xmlns:gmd="http://www.isotc211.org/2005/gmd"' +
+          '                             xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
+          '                             xmlns:geonet="http://www.fao.org/geonetwork"' +
+          '                             gco:isoType="gmd:CI_ResponsibleParty">' +
+          '   <gmd:organisationName xsi:type="gmd:PT_FreeText_PropertyType">' +
+          '      <gmd:PT_FreeText>' +
+          '         <gmd:textGroup>' +
+          '            <gmd:LocalisedCharacterString locale="#DE">~~ Template First Name ~~</gmd:LocalisedCharacterString>' +
+          '         </gmd:textGroup>' +
+          '         <gmd:textGroup>' +
+          '            <gmd:LocalisedCharacterString locale="#FR">~~ Template First Name ~~</gmd:LocalisedCharacterString>' +
+          '         </gmd:textGroup>' +
+          '         <gmd:textGroup>' +
+          '            <gmd:LocalisedCharacterString locale="#IT">~~ Template First Name ~~</gmd:LocalisedCharacterString>' +
+          '         </gmd:textGroup>' +
+          '         <gmd:textGroup>' +
+          '            <gmd:LocalisedCharacterString locale="#EN">~~ Template First Name ~~</gmd:LocalisedCharacterString>' +
+          '         </gmd:textGroup>' +
+          '      </gmd:PT_FreeText>' +
+          '   </gmd:organisationName>' +
+          '   <gmd:contactInfo>' +
+          '      <gmd:CI_Contact>' +
+          '         <gmd:phone>' +
+          '            <che:CHE_CI_Telephone gco:isoType="gmd:CI_Telephone">' +
+          '               <gmd:voice>' +
+          '                  <gco:CharacterString/>' +
+          '               </gmd:voice>' +
+          '            </che:CHE_CI_Telephone>' +
+          '         </gmd:phone>' +
+          '         <gmd:address>' +
+          '            <che:CHE_CI_Address gco:isoType="gmd:CI_Address">' +
+          '               <gmd:city>' +
+          '                  <gco:CharacterString/>' +
+          '               </gmd:city>' +
+          '               <gmd:electronicMailAddress>' +
+          '                  <gco:CharacterString>~~ Template Email ~~</gco:CharacterString>' +
+          '               </gmd:electronicMailAddress>' +
+          '               <che:streetName>' +
+          '                  <gco:CharacterString/>' +
+          '               </che:streetName>' +
+          '               <che:streetNumber>' +
+          '                  <gco:CharacterString/>' +
+          '               </che:streetNumber>' +
+          '            </che:CHE_CI_Address>' +
+          '         </gmd:address>' +
+          '      </gmd:CI_Contact>' +
+          '  </gmd:contactInfo>' +
+          '   <gmd:role>' +
+          '      <gmd:CI_RoleCode codeList="http://www.isotc211.org/2005/resources/codeList.xml#CI_RoleCode"' +
+          '                       codeListValue="pointOfContact"/>' +
+          '  </gmd:role>' +
+          '   <che:individualFirstName>' +
+          '      <gco:CharacterString>~~ Template First Name ~~</gco:CharacterString>' +
+          '  </che:individualFirstName>' +
+          '   <che:individualLastName>' +
+          '      <gco:CharacterString>~~ Template Last Name ~~</gco:CharacterString>' +
+          '  </che:individualLastName>' +
+          '</che:CHE_CI_ResponsibleParty>'
         };
       }]);
 })();
