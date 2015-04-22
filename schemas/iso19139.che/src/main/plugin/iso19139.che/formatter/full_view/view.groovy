52ae0d9f55ce1838fe6b9c288edb7fb643526847
Jesse Eichar <jesse.eichar@camptocamp.com>
2015-04-22 18:48:39 +0200

    hide iso19139 topic categories in iso19139.che metadata

--- schemas/iso19139.che/src/main/plugin/iso19139.che/formatter/full_view/view.groovy
+++ schemas/iso19139.che/src/main/plugin/iso19139.che/formatter/full_view/view.groovy
@@ -4,3 +4,9 @@ def isoHandlers = new iso19139che.Handlers(handlers, f, env)
 SummaryFactory.summaryHandler({it.parent() is it.parent()}, isoHandlers)
 
 isoHandlers.addDefaultHandlers()
+
+def iso19139TopicCategories = ['planningCadastre', 'environment', 'geoscientificInformation', 'imageryBaseMapsEarthCover', 'utilitiesCommunication']
+handlers.add name: 'gmd:topicCategory', select: 'gmd:topicCategory', group: true, priority: 5, { elems ->
+    def listItems = elems.findAll{!it.text().isEmpty() && !iso19139TopicCategories.contains(it.text())}.collect {f.codelistValueLabel("MD_TopicCategoryCode", it.text())};
+    handlers.fileResult("html/list-entry.html", [label:f.nodeLabel(elems[0]), listItems: listItems])
+}
