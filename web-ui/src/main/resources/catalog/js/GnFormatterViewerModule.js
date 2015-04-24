edcb5b90d653d2be1535cf0e3ecbea816bf1379b
Jesse Eichar <jesse.eichar@camptocamp.com>
2015-04-24 21:54:41 +0200

    Add metadata  actions menu to metadata viewer

--- web-ui/src/main/resources/catalog/js/GnFormatterViewerModule.js
+++ web-ui/src/main/resources/catalog/js/GnFormatterViewerModule.js
@@ -6,6 +6,13 @@
   goog.require('gn_catalog_service');
   goog.require('gn_formatter_lib');
   goog.require('gn_utility_directive');
+  goog.require('gn_popup_directive');
+  // GEOCAT
+  goog.require('gn_search_geocat_mdactionmenu');
+  goog.require('gn_mdactions_service');
+  goog.require('gn_alert');
+  goog.require('gn_popup_service');
+  // END GEOCAT
 
 
 
@@ -14,9 +21,11 @@
 
 
 
-
+  // GEOCAT
   var module = angular.module('gn_formatter_viewer',
-      ['ngRoute', 'gn', 'gn_utility_directive', 'gn_catalog_service']);
+      ['ngRoute', 'gn', 'gn_utility_directive', 'gn_catalog_service', 'gn_search_geocat_mdactionmenu',
+        'gn_popup_service', 'gn_mdactions_service', 'gn_alert']);
+  // END GEOCAT
 
   // Define the translation files to load
   module.constant('$LOCALES', ['core']);
@@ -24,6 +33,9 @@
   module.controller('GnFormatterViewer',
       ['$scope', '$http', '$sce', '$routeParams',
        function($scope, $http, $sce, $routeParams) {
+         $scope.md = {
+           'geonet:info': {}
+         };
          $scope.metadata = '';
          $scope.loading = true;
 
@@ -38,6 +50,10 @@
            $scope.loading = undefined;
            $scope.metadata = $sce.trustAsHtml(data);
          });
+         $http.get('qi?_content_type=json&fast=index&_id=' + mdId).success(function(data){
+           angular.copy(data.metadata, $scope.md);
+         });
+
        }]);
   module.config(['$routeProvider', function($routeProvider) {
     var tpls = '../../catalog/templates/';
