ab04f49bb0c966b3fb9de71649260e963c58e63f
Jesse Eichar <jesse.eichar@camptocamp.com>
2015-05-03 17:42:17 +0200

    Fix a few minor issues with keyword lastModified calculations.

--- services/src/main/java/org/fao/geonet/services/thesaurus/GetKeywords.java
+++ services/src/main/java/org/fao/geonet/services/thesaurus/GetKeywords.java
@@ -203,7 +203,8 @@ public class GetKeywords {
 
 	}
 
-	private KeywordSearchParamsBuilder parseBuilder(String uiLang, String searchTerm, String maxResults, String offset, List<String> targetLangs, List<String> thesauri, String thesauriDomainName,
+	private KeywordSearchParamsBuilder parseBuilder(String uiLang, String searchTerm, String maxResults, String offset,
+													List<String> targetLangs, List<String> thesauri, String thesauriDomainName,
 													String typeSearch, String keywordUriCode, IsoLanguagesMapper mapper) {
 		KeywordSearchParamsBuilder parsedParams = new KeywordSearchParamsBuilder(mapper).lenient(true);
 
