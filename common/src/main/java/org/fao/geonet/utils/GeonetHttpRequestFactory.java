a7fb997f27c7e5ceafca89350a87969c599671e6
Jesse Eichar <jesse.eichar@camptocamp.com>
2015-09-18 11:11:58 +0200

    remove print lines

--- common/src/main/java/org/fao/geonet/utils/GeonetHttpRequestFactory.java
+++ common/src/main/java/org/fao/geonet/utils/GeonetHttpRequestFactory.java
@@ -255,18 +255,15 @@ public class GeonetHttpRequestFactory {
 // set up a TrustManager that trusts everything
             sslContext.init(null, new TrustManager[]{new X509TrustManager() {
                 public X509Certificate[] getAcceptedIssuers() {
-                    System.out.println("getAcceptedIssuers =============");
                     return null;
                 }
 
                 public void checkClientTrusted(X509Certificate[] certs,
                                                String authType) {
-                    System.out.println("checkClientTrusted =============");
                 }
 
                 public void checkServerTrusted(X509Certificate[] certs,
                                                String authType) {
-                    System.out.println("checkServerTrusted =============");
                 }
             }}, new SecureRandom());
 
