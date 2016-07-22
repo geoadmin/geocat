4cdd2c63ec777f5b2ac3438a057545260389002d
Julien Acroute <julien.acroute@camptocamp.com>
2016-07-22 15:56:24 +0200

    Change base layer of ol3 map

--- web-ui/src/main/resources/catalog/views/geocat/config.js
+++ web-ui/src/main/resources/catalog/views/geocat/config.js
@@ -43,49 +43,55 @@
         })
       };
 
-      var resolutions = [
-        4000, 3750, 3500, 3250, 3000, 2750, 2500, 2250, 2000, 1750, 1500, 1250,
-        1000, 750, 650, 500, 250, 100, 50, 20, 10, 5, 2.5, 2, 1.5, 1, 0.5
-      ];
-
-      var matrixIds = [];
-      for (var i = 0; i < resolutions.length; i++) {
-        matrixIds.push(i);
+      var projection = ol.proj.get('EPSG:3857');
+      var projectionExtent = projection.getExtent();
+      var size = ol.extent.getWidth(projectionExtent) / 256;
+      var resolutions = new Array(14);
+      var matrixIds = new Array(14);
+      for (var z = 0; z < 14; ++z) {
+        // generate resolutions and matrixIds arrays for this WMTS
+        resolutions[z] = size / Math.pow(2, z);
+        matrixIds[z] = z;
       }
 
-      var tileGrid = new ol.tilegrid.WMTS({
-        origin: [420000, 350000],
-        resolutions: resolutions,
-        matrixIds: matrixIds
-      });
+      var defaultUrl = 'https://server.arcgisonline.com/arcgis/rest/services/World_Topo_Map/MapServer/WMTS/tile/1.0.0/World_Topo_Map/default/default028mm/{TileMatrix}/{TileRow}/{TileCol}.jpg';
 
-      var defaultUrl = '//wmts{5-9}.geo.admin.ch/1.0.0/{Layer}/default/' +
-          '20140520/21781/' +
-          '{TileMatrix}/{TileRow}/{TileCol}.jpeg';
       var url = angular.isDefined(searchSettings.mapConfig) ?
           searchSettings.mapConfig.url || defaultUrl : defaultUrl;
 
       var chLayer = new ol.layer.Tile({
-        source: new ol.source.WMTS(({
-          crossOrigin: 'anonymous',
-          url: url,
-          tileGrid: tileGrid,
-          layer: 'ch.swisstopo.pixelkarte-farbe',
+        source: new ol.source.WMTS({
+          url: defaultUrl,
+          tileGrid: new ol.tilegrid.WMTS({
+            origin: ol.extent.getTopLeft(projectionExtent),
+            resolutions: resolutions,
+            matrixIds: matrixIds
+          }),
           requestEncoding: 'REST',
-          projection: 'EPSG:21781'
-        })),
-        extent: [434250, 37801.909073720046, 894750, 337801.90907372005],
-        useInterimTilesOnError: false
+          wrapX: true
+        })
       });
+
+      var mousePositionControl = new ol.control.MousePosition({
+        coordinateFormat: ol.coordinate.createStringXY(4),
+        projection: 'EPSG:3857',
+        // comment the following two lines to have the mouse position
+        // be placed within the map.
+        className: 'custom-mouse-position',
+        target: document.getElementById('mouse-position'),
+        undefinedHTML: '&nbsp;'
+      });
+
       searchSettings.searchMap = new ol.Map({
+        controls: ol.control.defaults({
+          attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+            collapsible: false
+          })
+        }).extend([mousePositionControl]),
         layers: [chLayer],
         view: new ol.View({
-          resolutions: [1250, 1000, 750, 650, 500, 250, 100, 50, 20,
-            10, 5, 2.5, 2, 1, 0.5, 0.25, 0.1],
-          extent: [420000, 30000, 900000, 350000],
-          projection: 'EPSG:21781',
-          center: [660000, 190000],
-          zoom: 2
+          center: [929317, 5909466],
+          zoom: 7
         })
       });
 
