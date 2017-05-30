ecf856986e2cf661f93b260c131bc95ff3b59423
Florent gravin <florent.gravin@camptocamp.com>
2017-05-30 13:33:23 +0200

    Add script for schema update you can move schemas into webapp while jetty is running

---
+++ migrate_schemas.sh
@@ -0,0 +1,4 @@
+cp -R -f schemas/iso19139/src/main/plugin/iso19139/ web/src/main/webapp/WEB-INF/data/config/schema_plugins/
+cp -R -f schemas/iso19139.che/src/main/plugin/iso19139.che/ web/src/main/webapp/WEB-INF/data/config/schema_plugins/
+cp -R -f schemas/dublin-core/src/main/plugin/dublin-core/ web/src/main/webapp/WEB-INF/data/config/schema_plugins/
+
