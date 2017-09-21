27bf0919b06b62530d3ab916afc6283d46a2cb94
Florent gravin <florent.gravin@camptocamp.com>
2017-09-21 11:35:55 +0200

    Port back metadata result lit template

--- web-ui/src/main/resources/catalog/views/geocat/module.js
+++ web-ui/src/main/resources/catalog/views/geocat/module.js
@@ -25,20 +25,102 @@
 
   goog.provide('gn_search_geocat');
 
-
-
-  
   goog.require('gn_search_default');
+  goog.require('gn_search_geocat_mdactionmenu');
 
-  var module = angular.module('gn_search_geocat',
-      ['gn_search_default']);
+  var module = angular.module('gn_search_geocat',[
+    'gn_search_default',
+    'gn_search_geocat_mdactionmenu'
+  ]);
 
   module.controller('gnsGeocat', [
     '$scope',
     '$controller',
     function($scope, $controller) {
       angular.extend(this, $controller('gnsDefault', { $scope: $scope }));
-      $scope.resultTemplate = '../../catalog/views/geocat/templates/geocat.html';
+      $scope.resultTemplate = '../../catalog/views/geocat/templates/list.html';
     }
   ]);
+
+  module.directive('gcFixMdlinks', [
+    function() {
+
+      return {
+        restrict: 'A',
+        scope: false,
+        link: function(scope) {
+          var links = scope.md.getLinksByType('LINK', 'CHTOPO:specialised-geoportal', "OGC:WFS", "OGC:WMTS");
+          scope.links = [];
+          scope.downloads = scope.md.getLinksByType('DOWNLOAD', 'FILE');
+
+          if (angular.isDefined(scope.md.type) && scope.md.type.indexOf('service') >= 0) {
+            scope.layers = [];
+            if (angular.isDefined(scope.md.wmsuri) && !angular.isArray(scope.md.wmsuri)) {
+              scope.md.wmsuri = [scope.md.wmsuri];
+            }
+            angular.forEach(scope.md.wmsuri, function(uri) {
+              var e = uri.split('###');
+              scope.layers.push({
+                uuid: e[0],
+                name: e[1],
+                desc: e[1],
+                url: e[2]
+              });
+            });
+          } else {
+            scope.layers = scope.md.getLinksByType('OGC:WMS', 'kml');
+          }
+
+          angular.forEach(links, function(l) {
+            if(l.url && l.url != '' && l.url != '-') {
+              if(l.desc == '' || l.desc == '-') {
+                l.desc = l.url;
+              }
+              scope.links.push(l);
+            }
+          });
+
+          angular.forEach(scope.downloads, function(l) {
+            if(l.url && l.url != '' && l.url != '-') {
+              if(l.desc == '' || l.desc == '-') {
+                l.desc = l.url;
+              }
+            }
+          });
+
+          angular.forEach(scope.layers, function(l) {
+            if(l.url && l.url != '' && l.url != '-') {
+              if(l.desc == '' || l.desc == '-') {
+                l.desc = l.url;
+              }
+            }
+          });
+
+          var d;
+          if (scope.md['geonet:info'].changeDate) {
+            d = {
+              date: scope.md['geonet:info'].changeDate,
+              type: 'changeDate'
+            };
+          }
+          else if (scope.md['geonet:info'].publishedDate) {
+            d = {
+              date: scope.md['geonet:info'].publishedDate,
+              type: 'changeDate'
+            };
+          }
+          else if (scope.md['geonet:info'].createDate) {
+            d = {
+              date: scope.md['geonet:info'].createDate,
+              type: 'createDate'
+            };
+          }
+          scope.showDate = {
+            date: moment(d).format('YYYY-MM-DD'),
+            type: d.type
+          };
+        }
+      };
+    }]);
+
 })();
