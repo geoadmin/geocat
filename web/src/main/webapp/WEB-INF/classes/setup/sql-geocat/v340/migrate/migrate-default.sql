3c164ea270cd6648da2f3480f1e86f4d634b2b48
Florent gravin <florent.gravin@camptocamp.com>
2017-11-21 17:03:50 +0100

    extent: flip coordinates of 4326 geocat custom extent

--- web/src/main/webapp/WEB-INF/classes/setup/sql-geocat/v340/migrate/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-geocat/v340/migrate/migrate-default.sql
@@ -334,7 +334,7 @@ INSERT INTO metadata (
       COALESCE((xpath(
       '//EN/text()',
       concat('<root>', replace(replace("DESC", ']]>', ''), '<![CDATA[', ''), '</root>')::xml)::varchar[])[1], '') AS endesc,
-      ST_AsGML(3, ST_Transform(ST_Force2D(the_geom), 4326))  gml,
+      ST_AsGML(3, ST_FlipCoordinates(ST_Transform(ST_Force2D(the_geom), 4326)))  gml,
       ST_XMin(ST_Transform(the_geom, 4326)) AS minx,
       ST_YMin(ST_Transform(the_geom, 4326)) AS miny,
       ST_XMax(ST_Transform(the_geom, 4326)) AS maxx,
