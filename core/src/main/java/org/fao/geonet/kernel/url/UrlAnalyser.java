5dbdbb7aec7715cb5efec745369f0bccd01e1088
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2019-06-27 16:35:31 +0200

    relocate analyser process

--- core/src/main/java/org/fao/geonet/kernel/url/UrlAnalyser.java
+++ core/src/main/java/org/fao/geonet/kernel/url/UrlAnalyser.java
@@ -1,4 +1,53 @@
 package org.fao.geonet.kernel.url;
 
+import org.fao.geonet.domain.AbstractMetadata;
+import org.fao.geonet.domain.Link;
+import org.fao.geonet.domain.MetadataLink;
+import org.fao.geonet.kernel.SchemaManager;
+import org.fao.geonet.kernel.schema.LinkAwareSchemaPlugin;
+import org.fao.geonet.kernel.schema.LinkPatternStreamer.ILinkBuilder;
+import org.fao.geonet.kernel.schema.SchemaPlugin;
+import org.fao.geonet.repository.MetadataRepository;
+import org.jdom.Element;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 public class UrlAnalyser {
+
+    @Autowired
+    private SchemaManager schemaManager;
+
+    @Autowired
+    private MetadataRepository metadataRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void processMetadata(Element element, AbstractMetadata md) throws org.jdom.JDOMException {
+        SchemaPlugin schemaPlugin = schemaManager.getSchema(md.getDataInfo().getSchemaId()).getSchemaPlugin();
+        if (schemaPlugin instanceof LinkAwareSchemaPlugin) {
+
+            ((LinkAwareSchemaPlugin) schemaPlugin).create(new ILinkBuilder<Link, AbstractMetadata>() {
+                @Override
+                public Link build() {
+                    return new Link();
+                }
+
+                @Override
+                public void setUrl(Link link, String url) {
+                    link.setUrl(url);
+                }
+
+                @Override
+                public void persist(Link link, AbstractMetadata metadata) {
+                    MetadataLink metadataLink = new MetadataLink();
+                    entityManager.persist(link);
+                    metadataLink.setId(metadataRepository.findOne(metadata.getId()), link);
+                    entityManager.persist(metadataLink);
+                }
+            }).processAllRawText(element, md);
+        }
+    }
 }
