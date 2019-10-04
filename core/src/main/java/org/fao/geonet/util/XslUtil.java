b047de6ec53de0e42f7efc21f8b905756b2904e3
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2019-10-04 15:13:20 +0200

    mutualise urlchecker (use it also when schematron) and consider http 404 on HEAD as 405

--- core/src/main/java/org/fao/geonet/util/XslUtil.java
+++ core/src/main/java/org/fao/geonet/util/XslUtil.java
@@ -50,6 +50,7 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.fao.geonet.ApplicationContextHolder;
 import org.fao.geonet.SystemInfo;
 import org.fao.geonet.constants.Geonet;
+import org.fao.geonet.domain.LinkStatus;
 import org.fao.geonet.domain.User;
 import org.fao.geonet.exceptions.JeevesException;
 import org.fao.geonet.kernel.DataManager;
@@ -60,6 +61,7 @@ import org.fao.geonet.kernel.search.LuceneSearcher;
 import org.fao.geonet.kernel.search.Translator;
 import org.fao.geonet.kernel.setting.SettingInfo;
 import org.fao.geonet.kernel.setting.SettingManager;
+import org.fao.geonet.kernel.url.UrlChecker;
 import org.fao.geonet.languages.IsoLanguagesMapper;
 import org.fao.geonet.lib.Lib;
 import org.fao.geonet.repository.UserRepository;
@@ -482,7 +484,6 @@ public final class XslUtil {
         }
     }
 
-
     /**
      * Returns the HTTP code  or error message if error occurs during URL connection.
      *
@@ -490,63 +491,12 @@ public final class XslUtil {
      * @return the numeric code of the HTTP request or a String with an error.
      */
     public static String getUrlStatus(String url) {
-        return getUrlStatus(url, 5);
-
-    }
-
-    /**
-     * Returns the HTTP code  or error message if error occurs during URL connection.
-     *
-     * @param url       The URL to ckeck.
-     * @param tryNumber the number of remaining tries.
-     */
-    public static String getUrlStatus(String url, int tryNumber) {
-        if (tryNumber < 1) {
-            // protect against redirect loops
-            return "ERR_TOO_MANY_REDIRECTS";
+        UrlChecker urlChecker = ApplicationContextHolder.get().getBean(UrlChecker.class);
+        LinkStatus urlStatus = urlChecker.getUrlStatus(url);
+        if (urlStatus.getStatusValue().equalsIgnoreCase("4XX") || urlStatus.getStatusValue().equalsIgnoreCase("310")) {
+           return urlStatus.getStatusInfo();
         }
-        HttpHead head = new HttpHead(url);
-        GeonetHttpRequestFactory requestFactory = ApplicationContextHolder.get().getBean(GeonetHttpRequestFactory.class);
-        ClientHttpResponse response = null;
-        try {
-            response = requestFactory.execute(head, new Function<HttpClientBuilder, Void>() {
-                @Nullable
-                @Override
-                public Void apply(@Nullable HttpClientBuilder originalConfig) {
-                    RequestConfig.Builder config = RequestConfig.custom()
-                        .setConnectTimeout(1000)
-                        .setConnectionRequestTimeout(3000)
-                        .setSocketTimeout(5000);
-                    RequestConfig requestConfig = config.build();
-                    originalConfig.setDefaultRequestConfig(requestConfig);
-
-                    return null;
-                }
-            });
-            //response = requestFactory.execute(head);
-            if (response.getRawStatusCode() == HttpStatus.SC_BAD_REQUEST
-                || response.getRawStatusCode() == HttpStatus.SC_METHOD_NOT_ALLOWED
-                || response.getRawStatusCode() == HttpStatus.SC_INTERNAL_SERVER_ERROR) {
-                // the website doesn't support HEAD requests. Need to do a GET...
-                response.close();
-                HttpGet get = new HttpGet(url);
-                response = requestFactory.execute(get);
-            }
-
-            if (response.getStatusCode().is3xxRedirection() && response.getHeaders().containsKey("Location")) {
-                // follow the redirects
-                return getUrlStatus(response.getHeaders().getFirst("Location"), tryNumber - 1);
-            }
-
-            return String.valueOf(response.getRawStatusCode());
-        } catch (IOException e) {
-            Log.error(Geonet.GEONETWORK, "IOException validating  " + url + " URL. " + e.getMessage(), e);
-            return e.getMessage();
-        } finally {
-            if (response != null) {
-                response.close();
-            }
-        }
+        return urlStatus.getStatusValue();
     }
 
     public static String threeCharLangCode(String langCode) {
