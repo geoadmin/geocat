d711af4e834cc656bb3fae1dac19960ed6dc2f03
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2020-03-06 13:19:16 +0100

    no need for service context here ? (and no exception to be thrown)

--- services/src/main/java/org/fao/geonet/api/records/formatters/FormatterApi.java
+++ services/src/main/java/org/fao/geonet/api/records/formatters/FormatterApi.java
@@ -364,7 +364,7 @@ public class FormatterApi extends AbstractFormatService implements ApplicationLi
             if (request.checkNotModified(language, roundedChangeDate) &&
                 cacheConfig.allowCaching(key)) {
                 if (!skipPopularityBool) {
-                    metadataUtils.increasePopularity(context, String.valueOf(metadata.getId()));
+                    metadataUtils.increasePopularity(String.valueOf(metadata.getId()));
                 }
                 return;
             }
@@ -389,7 +389,7 @@ public class FormatterApi extends AbstractFormatService implements ApplicationLi
         }
         if (bytes != null) {
             if (!skipPopularityBool) {
-                metadataUtils.increasePopularity(context, String.valueOf(metadata.getId()));
+                metadataUtils.increasePopularity(String.valueOf(metadata.getId()));
             }
             writeOutResponse(context, metadataUuid,
                 LanguageUtils.locale2gnCode(locale.getISO3Language()),
@@ -529,7 +529,7 @@ public class FormatterApi extends AbstractFormatService implements ApplicationLi
             long roundedChangeDate = changeDateAsTime / 1000 * 1000;
             if (request.checkNotModified(roundedChangeDate) && cacheConfig.allowCaching(key)) {
                 if (!skipPopularityBool) {
-                    metadataUtils.increasePopularity(context, resolvedId);
+                    metadataUtils.increasePopularity(resolvedId);
                 }
                 return;
             }
@@ -555,7 +555,7 @@ public class FormatterApi extends AbstractFormatService implements ApplicationLi
         }
         if (bytes != null) {
             if (!skipPopularityBool) {
-                metadataUtils.increasePopularity(context, resolvedId);
+                metadataUtils.increasePopularity(resolvedId);
             }
 
             writeOutResponse(context, resolvedId, lang, request.getNativeResponse(HttpServletResponse.class), formatType, bytes);
@@ -647,7 +647,7 @@ public class FormatterApi extends AbstractFormatService implements ApplicationLi
 
     @VisibleForTesting
     Pair<FormatterImpl, FormatterParams> loadMetadataAndCreateFormatterAndParams(ServiceContext context, Key key, final NativeWebRequest request) throws Exception {
-        final Pair<Element, AbstractMetadata> elementMetadataPair = getMetadata(context, key.mdId, key.hideWithheld);
+        final Pair<Element, AbstractMetadata> elementMetadataPair = getMetadata(key.mdId, key.hideWithheld);
         Element metadata = elementMetadataPair.one();
         AbstractMetadata metadataInfo = elementMetadataPair.two();
 
@@ -728,9 +728,7 @@ public class FormatterApi extends AbstractFormatService implements ApplicationLi
         return isInSchemaPlugin;
     }
 
-    public Pair<Element, AbstractMetadata> getMetadata(ServiceContext context, int id,
-                                               Boolean hide_withheld) throws Exception {
-
+    public Pair<Element, AbstractMetadata> getMetadata(int id, Boolean hide_withheld) throws Exception {
         AbstractMetadata md = loadMetadata(metadataUtils, id);
         boolean doXLinks = xmlSerializer.resolveXLinks();
 
