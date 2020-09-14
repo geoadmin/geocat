5bfc1388df263c9539bc0e005bb7493f66cb955c
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2020-09-14 10:54:46 +0200

    avoid 'A collection with cascade=all-delete-orphan was no longer referenced by the owning entity instance' trouble

--- services/src/main/java/org/fao/geonet/api/links/LinksApi.java
+++ services/src/main/java/org/fao/geonet/api/links/LinksApi.java
@@ -78,6 +78,8 @@ import springfox.documentation.annotations.ApiIgnore;
 
 import javax.annotation.PostConstruct;
 import javax.management.MalformedObjectNameException;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -125,6 +127,9 @@ public class LinksApi {
     AccessManager accessManager;
     @Autowired
     OperationAllowedRepository operationAllowedRepository;
+    @PersistenceContext
+    EntityManager em;
+
     private ArrayDeque<SelfNaming> mAnalyseProcesses = new ArrayDeque<>(NUMBER_OF_SUBSEQUENT_PROCESS_MBEAN_TO_KEEP);
 
     @PostConstruct
@@ -408,7 +413,9 @@ public class LinksApi {
                     }
                     return false;
                 }).collect(Collectors.toSet());
-                l.setRecords(mdl);
+                em.detach(l);
+                l.getRecords().clear();
+                l.getRecords().addAll(mdl);
             });
         }
 
@@ -423,7 +430,9 @@ public class LinksApi {
                     }
                     return false;
                 }).collect(Collectors.toSet());
-                l.setRecords(mdl);
+                em.detach(l);
+                l.getRecords().clear();
+                l.getRecords().addAll(mdl);
             });
         }
 
@@ -443,7 +452,9 @@ public class LinksApi {
                     }
                     return false;
                 }).collect(Collectors.toSet());
-                l.setRecords(mdl);
+                em.detach(l);
+                l.getRecords().clear();
+                l.getRecords().addAll(mdl);
             });
         }
 
