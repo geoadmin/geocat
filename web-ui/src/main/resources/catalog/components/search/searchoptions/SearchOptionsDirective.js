1852484bf62bd9fe01fd58e4dfb8b0f9a0fe2636
christophe mangeat <christophe.mangeat@camptocamp.com>
2022-03-10 16:46:12 +0100

    [geocat]: ugly patch as don't want 'fre' or 'deu' to be displayed during demo

--- web-ui/src/main/resources/catalog/components/search/searchoptions/SearchOptionsDirective.js
+++ web-ui/src/main/resources/catalog/components/search/searchoptions/SearchOptionsDirective.js
@@ -87,6 +87,7 @@
                     scope.languagesStats[i.key] = i.doc_count;
                   });
                   scope.languagesAvailable = Object.keys(scope.languagesStats);
+                  scope.languagesAvailable= scope.languagesAvailable.filter(function(x) {return x != 'deu'}).filter(function(x) {return x != 'fra'});
                   controller.setLanguageWhiteList(scope.languagesAvailable);
                 });
             }
