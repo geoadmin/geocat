5e548b596f0f2b72a0698573ab5c514bd638c9f4
christophe mangeat <christophe.mangeat@camptocamp.com>
2022-08-26 15:44:58 +0200

    replace 20140520 by current as not recognized anymore

--- web-ui/src/main/resources/catalog/components/common/map/mapService.js
+++ web-ui/src/main/resources/catalog/components/common/map/mapService.js
@@ -1697,7 +1697,7 @@
             });
 
             var defaultUrl = 'https://wmts{5-9}.geo.admin.ch/1.0.0/{Layer}/default/' +
-              '20140520/21781/' +
+              'current/21781/' +
               '{TileMatrix}/{TileRow}/{TileCol}.jpeg';
 
             return new ol.layer.Tile({
