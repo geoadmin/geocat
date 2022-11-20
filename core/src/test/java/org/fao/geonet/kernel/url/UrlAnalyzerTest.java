a59018caf50897dfe9b46df898687d4d3a3ba0fa
christophe mangeat <christophe.mangeat@camptocamp.com>
2022-11-20 23:14:58 +0100

    [gn, merged]: fix one integration test

--- core/src/test/java/org/fao/geonet/kernel/url/UrlAnalyzerTest.java
+++ core/src/test/java/org/fao/geonet/kernel/url/UrlAnalyzerTest.java
@@ -10,6 +10,7 @@ import org.fao.geonet.domain.MetadataLink;
 import org.fao.geonet.domain.MetadataType;
 import org.fao.geonet.kernel.SchemaManager;
 import org.fao.geonet.kernel.datamanager.IMetadataManager;
+import org.fao.geonet.kernel.setting.SettingManager;
 import org.fao.geonet.repository.LinkRepository;
 import org.fao.geonet.repository.LinkStatusRepository;
 import org.fao.geonet.repository.MetadataLinkRepository;
@@ -72,6 +73,9 @@ public class UrlAnalyzerTest extends AbstractCoreIntegrationTest {
 
     @Autowired
     private UrlChecker urlChecker;
+
+    @Autowired
+    protected SettingManager settingManager;
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -267,6 +271,7 @@ public class UrlAnalyzerTest extends AbstractCoreIntegrationTest {
         toTest.metadataLinkRepository = metadataLinkRepository;
         toTest.entityManager = entityManager;
         toTest.urlChecker = urlChecker;
+        toTest.settingManager = settingManager;
         return toTest;
     }
 
