96c057730f9089f3e99c50fda9b44d49d3843f47
christophe mangeat <christophe.mangeat@camptocamp.com>
2022-12-22 15:10:09 +0100

    MGEO_SB-820 tests

--- core/src/test/java/org/fao/geonet/kernel/LocalXLinksUpdateDeleteTest.java
+++ core/src/test/java/org/fao/geonet/kernel/LocalXLinksUpdateDeleteTest.java
@@ -14,7 +14,6 @@ import org.fao.geonet.utils.Xml;
 import org.jdom.Attribute;
 import org.jdom.Element;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -33,7 +32,6 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
 
-@Ignore
 public class LocalXLinksUpdateDeleteTest extends AbstractIntegrationTestWithMockedSingletons {
 
     private static final int TEST_OWNER = 42;
