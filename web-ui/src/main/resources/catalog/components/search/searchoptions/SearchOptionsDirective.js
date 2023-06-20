daef1e54c25ef78f2be6a7d373823654d4761532
sebr72 <sebastien_riollet@hotmail.com>
2023-06-20 14:17:50 +0200

    Apply prettier to changed files

--- web-ui/src/main/resources/catalog/components/search/searchoptions/SearchOptionsDirective.js
+++ web-ui/src/main/resources/catalog/components/search/searchoptions/SearchOptionsDirective.js
@@ -87,7 +87,13 @@
                     scope.languagesStats[i.key] = i.doc_count;
                   });
                   scope.languagesAvailable = Object.keys(scope.languagesStats);
-                  scope.languagesAvailable= scope.languagesAvailable.filter(function(x) {return x != 'deu'}).filter(function(x) {return x != 'fra'});
+                  scope.languagesAvailable = scope.languagesAvailable
+                    .filter(function (x) {
+                      return x != "deu";
+                    })
+                    .filter(function (x) {
+                      return x != "fra";
+                    });
                   controller.setLanguageWhiteList(scope.languagesAvailable);
                 });
             }
