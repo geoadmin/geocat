afa01a43a5aa3f0a481ddc2c233cc0987c27dc89
sebr72 <sebastien_riollet@hotmail.com>
2024-01-12 15:53:09 +0100

    Log orphanedDataPath into a File

--- services/src/main/java/org/fao/geonet/api/maintenance/DatadirCleaner.java
+++ services/src/main/java/org/fao/geonet/api/maintenance/DatadirCleaner.java
@@ -1,7 +1,10 @@
 package org.fao.geonet.api.maintenance;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.fao.geonet.kernel.GeonetworkDataDirectory;
+import org.fao.geonet.kernel.datamanager.IMetadataUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.stream.Stream;
 
 @RequestMapping(value = {"/{portal}/api/maintenance"})
@@ -24,6 +29,11 @@ public class DatadirCleaner {
 
     @Autowired
     GeonetworkDataDirectory geonetworkDataDirectory;
+    @Autowired
+    IMetadataUtils metadataUtils;
+
+    private BufferedWriter orphanedDataFile;
+    private Path orphanedDataFilePath;
 
     @io.swagger.v3.oas.annotations.Operation(summary = "Clean data dir")
     @RequestMapping(
@@ -33,18 +43,37 @@ public class DatadirCleaner {
     @ResponseStatus(value = HttpStatus.OK)
     @PreAuthorize("hasAuthority('UserAdmin')")
     @ResponseBody
-    public Object cleanDataDir() {
-        cleanFile();
-        return "cleaned";
+    public ObjectNode cleanDataDir() throws IOException {
+        return cleanFile();
     }
 
-    public void cleanFile() {
+    public ObjectNode cleanFile() throws IOException {
         Path rootPath = geonetworkDataDirectory.getMetadataDataDir();
+        orphanedDataFilePath = rootPath.resolve("orphanedDataFiles.txt");
+        try(BufferedWriter bw = Files.newBufferedWriter(orphanedDataFilePath, StandardOpenOption.WRITE)) {
+            orphanedDataFile = bw;
+            listFilesEatingException(rootPath) //
+                .flatMap(this::listFilesEatingException)
+                .filter(this::isOrphanedPath)
+                .forEach(this::deleteAndLogToFile);
 
-        listFilesEatingException(rootPath) //
-            .flatMap(this::listFilesEatingException)
-            .map(Path::toString)
-            .forEach(System.err::println);
+            ObjectNode status = new ObjectMapper().createObjectNode();
+            status.put("status", "Cleaned the orphaned data: see details in " + orphanedDataFilePath.toString());
+            return status;
+        }
+    }
+
+    private void deleteAndLogToFile(Path path) {
+        try {
+            orphanedDataFile.append(path.toAbsolutePath().toString());
+            orphanedDataFile.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean isOrphanedPath(Path path) {
+        return !metadataUtils.exists(Integer.parseInt(path.getFileName().toString()));
     }
 
     private Stream<Path> listFilesEatingException(Path path) {
